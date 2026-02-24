5ef321c2442f08227178bd877eb81e08e913f02e
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-02-23 23:25:25 -0300

    Primeiro exercicio, utilizando encapsulamento para exibir informações dos atributos privados

--- encapsulamento/src/Main.java
+++ encapsulamento/src/Main.java
@@ -1,5 +1,14 @@
+import Model.Carro;
+
 class Main{
     static void main(String[] args) {
-        System.out.println("teste");
+        System.out.println("Ex01");
+
+        Carro car = new Carro("Tracker","AYN-345",2025);
+
+        System.out.println("Veículo cadastrado:");
+        System.out.println("Modelo: " + car.getModelo());
+        System.out.println("Placa: " + car.getPlaca());
+        System.out.println("Ano: " + car.getAno());
     }
 }
